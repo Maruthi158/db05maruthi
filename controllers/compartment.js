7847cb25642f369172582f27c609756befb0f659
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-04-07 13:54:11 -0500

    Commiting Assignment11

--- controllers/compartment.js
+++ controllers/compartment.js
@@ -30,12 +30,32 @@ exports.compartment_update_put = function (req, res) {
 
 // VIEWS
 // Handle a show all view
-exports.compartment_view_all_Page = async function(req, res) {
-    try{
-    thecompartment = await compartment.find();
-    res.render('compartment', { title: 'compartment Search Results', results: thecompartment });
+exports.compartment_view_all_Page = async function (req, res) {
+    try {
+        thecompartment = await compartment.find();
+        res.render('compartment', { title: 'compartment Search Results', results: thecompartment });
     }
-    catch(err){
-    res.error(500,`{"error": ${err}}`);
+    catch (err) {
+        res.error(500, `{"error": ${err}}`);
     }
-    };
+};
+
+// Handle Costume create on POST.
+exports.compartment_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new compartment();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.compartment_name = req.body.compartment_name;
+    document.compartment_ID = req.body.compartment_ID;
+    document.compartment_type = req.body.compartment_type;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.error(500, `{"error": ${err}}`);
+    }
+};
