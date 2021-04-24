7400f31e4e437cbe583b68e2f28cb49f6a8e5658
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-04-23 20:34:57 -0500

    part 5

--- models/compartment.js
+++ models/compartment.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 const compartmentSchema = mongoose.Schema({
     compartment_name: String,
-    compartment_ID: Number,
+    compartment_ID: {
+        type: Number,
+
+        min:[1,"Minimum"],
+        max:[12,"Maximum"]
+    },
     compartment_type: String
 });
 
